49df5d4921ef39a1ce22a461263613aa0d23510d
Steve Akers <steve@steveakers.com>
2013-11-19 16:12:43 -0500

    initial commit

---
+++ hangout-alert.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/ruby
+
+require 'time'
+
+meetings = %x[/usr/local/bin/gcalcli agenda --calendar 'Your Calendar' --tsv]
+
+meetings.force_encoding("UTF-8").split("\n").each do |line|
+  row = line.split("\t")
+
+  title   = row[6]
+  start   = Time.parse(row[0] + " " + row[1])
+  details = row[4]
+  hangout = row[5]
+
+  time = Time.now
+  link = hangout.length > 0 ? "#{hangout}" : "#{details}"
+
+  if time <= start && time >= start - (5 * 60) 
+    msg = hangout.length > 0 ? "Hangout" : "Meeting"
+    %x[terminal-notifier -message '#{msg} starts at #{start.strftime('%l:%M')}.' -title '#{title}' -activate 'com.google.Chrome' -open '#{link}' -sound 'Basso']
+  end
+end
